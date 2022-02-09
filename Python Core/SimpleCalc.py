55f0c6aea8da7cd24de911732da4e73de4d23313
S-Osman4 <94788755+S-Osman4@users.noreply.github.com>
2022-02-09 17:51:12 +0300

    Finished Project

---
+++ Python Core/SimpleCalc.py
@@ -0,0 +1,18 @@
+'''
+Simple Calculator
+
+
+Write a program to take two integers as input and output their sum.
+
+Sample Input:
+2
+8
+
+Sample Output: 10
+'''
+# your code goes here
+a = int(input())
+b = int(input())
+
+sum = a+b
+print (sum) 
